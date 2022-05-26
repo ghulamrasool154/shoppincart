6faa8a7d99017a8caf98180412b34b3c878f9597
GhulamRasool <grsaddiqui75@gmail.com>
2022-05-26 19:55:08 +0500

    new create-ecommerce shopping cart

---
+++ src/components/Layout/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#8a2b06",
+        textAlign: "center",
+        fontFamily: "inherit",
+        color: "white",
+        lineHeight: "4rem",
+      }}
+    >
+      ReactMeals @ 2022
+    </div>
+  );
+};
+
+export default Footer;
